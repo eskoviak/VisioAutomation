8165fc5a0c54ee6657b75a413de34bba7973ccf7
Ed Skoviak <eskoviak@eskoviak.net>
2017-11-22 10:37:45 -0600

    EOD RWS Added module skeleton

---
+++ RWSVisioUtil.psm1
@@ -0,0 +1,21 @@
+<#
+  Module containing Visio automations for the EA Diagram Process
+#>
+
+#### Begin PUBLIC Funtions
+function New-Diagram {
+    param(
+    # Bring in the PowerShell defaults
+    [CmdletBinding()]
+    [Parameter()]
+    [String]$DiagramName
+    ) 
+    
+}
+
+Export-ModuleMember
+#### End PUBLIC Funtions
+
+#### Begin PRIVATE Funtions
+
+#### End PRIVATE Functions
