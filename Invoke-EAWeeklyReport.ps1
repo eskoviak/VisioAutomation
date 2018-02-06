1b009f5c4ca107d2f60122110cca82d28f075be1
Ed Skoviak <eskoviak@gmail.com>
2018-02-05 20:50:21 -0600

    getting further

--- Invoke-EAWeeklyReport.ps1
+++ Invoke-EAWeeklyReport.ps1
@@ -1,6 +1,7 @@
 #Import-Module ImportExcel
 
-$EAProjectListWorkbook = "http://rede.rwsc.net/teams/IT/EA/Documents/EA%20Project%20List.xlsx" # the name of the workbook
+$EAProjectListWorkbook = "http://redwingshoes.sharepoint.com/Shared%20Documents/EA%20Project%20List.xlsx"
+#$EAProjectListWorkbook = "http://rede.rwsc.net/teams/IT/EA/Documents/EA%20Project%20List.xlsx" # the name of the workbook
 $EAProjectListStatus = "Status"  # The name of the sheet with the status
 
 #$EAProjectList = Import-Excel -Path $EAProjectListWorkbook -WorksheetName $EAProjectListStatus
@@ -16,7 +17,7 @@ $excel.Visible = $false
 $range = $EAWorkSheet.UsedRange
 #Write-Host $range.Row
 foreach($row in $range.Rows) {
-    write-host $_.Row
+    write-host $("Row: {0}; ???: {1}" -f $row.Row, $($row.Item(1).Value[1]))
 }
 $EAWorkbook.Close($false)
 $excel.Quit()
