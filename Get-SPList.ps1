815eb0295ec345bf171baa7633db22176e3fbc29
Ed Skoviak <eskoviak@gmail.com>
2018-02-07 20:44:34 -0600

    added get-sp doesn't work

---
+++ Get-SPList.ps1
@@ -0,0 +1,35 @@
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint.Client")
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint.Client.Runtime")
+
+
+Function Get-SPOContext([string]$Url,[string]$UserName,[string]$Password)
+{
+    $SecurePassword = $Password | ConvertTo-SecureString -AsPlainText -Force
+    $context = New-Object Microsoft.SharePoint.Client.ClientContext($Url)
+    $context.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($UserName, $SecurePassword)
+    return $context
+}
+
+Function Get-ListItems([Microsoft.SharePoint.Client.ClientContext]$Context, [String]$ListTitle) {
+    $list = $Context.Web.Lists.GetByTitle($listTitle)
+    $qry = [Microsoft.SharePoint.Client.CamlQuery]::CreateAllItemsQuery()
+    $items = $list.GetItems($qry)
+    $Context.Load($items)
+    $Context.ExecuteQuery()
+    return $items 
+}
+
+
+
+$UserName = "eskoviak@eskoviak.net"
+$Password = Read-Host -Prompt "Enter the password"    
+$Url = "https://eskoviak.sharepoint.com/TeamSite/"
+
+
+$context = Get-SPOContext -Url $Url -UserName $UserName -Password $Password
+$items = Get-ListItems -Context $context -ListTitle "Activities" 
+foreach($item in $items)
+{
+   #...
+}
+$context.Dispose()
